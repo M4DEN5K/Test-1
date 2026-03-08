159c1d240a3ce4db9145e24afe4f08b9d7cc94ca
Andres Lopez <alope01@louisville.edu>
2026-03-08 19:14:27 -0400

    Test2

--- Main.py
+++ Main.py
@@ -1,5 +1,17 @@
 #This one is a personal project
 #Here I have a draft for a project :)
 
-print("This a calculator for vectors")
-print("Please a positive integer\nNegative numbers dont work")
+import math
+
+print("This a calculator for vectors\nThis program works with 3 vectors")
+print("\nPlease a positive integer\nNegative numbers dont work")
+
+vector=int(input("Enter a number: "))
+vector2=int(input("Enter a number: "))
+vector3=int(input("Enter a number: "))
+
+SUM=vector+vector2+vector3
+
+sqr=math.sqrt(vector)
+
+print("The sum is",sqr)
